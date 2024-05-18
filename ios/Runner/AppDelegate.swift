2ffc57c5e2a0907d1185e9e3c1c0e901716fc5ee
北野篤輝 <meisnit@gmail.com>
2024-05-18 23:38:06 +0900

    buiid. 1.0.0

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import BackgroundTasks
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,9 +9,41 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
+
+    if #available(iOS 13.0, *) {
+      BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.example.ogirealApp.processing", using: nil) { task in
+        self.handleBackgroundTask(task: task)
+      }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  @available(iOS 13.0, *)
+  func handleBackgroundTask(task: BGTask) {
+    if let processingTask = task as? BGProcessingTask {
+      processingTask.expirationHandler = {
+        processingTask.setTaskCompleted(success: false)
+      }
+      scheduleNextProcessingTask()
+      processingTask.setTaskCompleted(success: true)
+    }
+  }
+
+  @available(iOS 13.0, *)
+  func scheduleNextProcessingTask() {
+    let request = BGProcessingTaskRequest(identifier: "com.example.ogirealApp.processing")
+    request.requiresNetworkConnectivity = false
+    request.requiresExternalPower = false
+    
+    do {
+      try BGTaskScheduler.shared.submit(request)
+    } catch {
+      print("Could not schedule processing task: \(error)")
+    }
   }
 }
